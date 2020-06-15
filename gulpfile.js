998d4efbd1ef59a5ecd7e682992efed9dd722ccc
Isaev Igor <igor.igorovich.isaev@gmail.com>
2020-06-15 07:30:49 +0300

    2

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
 var sass = require('gulp-sass')
 var pipeline = require('readable-stream').pipeline;
 
-gulp.task('compress', function () {
-    return pipeline(
-        gulp.src('src/js/app.js'),
-        uglify(),
-        gulp.dest('dist/js')
-    );
-});
+
 
 
 gulp.task('scss',function () {
     return gulp.src('src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 
 
 
-gulp.task('watch', function () {
+gulp.task('default', gulp.series(['scss']));
 
-})
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.scss', gulp.series(['scss']));
+});
